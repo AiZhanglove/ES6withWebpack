095642bc0fb7c3473708df52417b048a92ba7f50
zhangai <zhangai@xiaomi.com>
2017-12-06 16:07:41 +0800

    add

---
+++ src/js/entry.js
@@ -0,0 +1,7 @@
+/**
+ * Created by ZhangAi on 2017/12/6.
+ */
+
+
+require('../index.css');
+console.log('1234')
--- webpack.config.js
+++ webpack.config.js
@@ -2,28 +2,43 @@ var webpack = require('webpack');
 module.exports = {
     //页面入口文件配置 （html文件引入唯一的js 文件）
     entry:{
-        main:'./src/index.js'
+        main:'./src/js/entry.js'
     },
     //对应输出项配置
     output:{
-        path:path.join(_dirname,'dist'), //入口文件最终要输出到哪里
-        filename:'[name].[hash:8].bundle.js', //输出文件的名称
+        path:__dirname+'/out', //入口文件最终要输出到哪里
+        filename:'index.js', //输出文件的名称
         publicPath:''  //公共资源路径
     },
     module:{
-        rules:[{
-            test:/\.css$/,
-            exclude:/node_modules/,
-            use:[
-                {
-                    loader:'style-loader'
-                },{
-                    loader:'css-loader'
-                }
-            ]
-        }]
+        rules:[
+            {
+                test:/.js$/,
+                use:['babel-loader']
+            },
+            //解析css, 并把css添加到html的style标签里
+            {
+                test:/.css$/,
+                use:['style-loader','css-loader']
+            },
+            //解析css, 并把css变成文件通过link标签引入
+            /*{
+                test:/.css$/,
+                use:ExtractTextPlugin.extract({fallback: 'style-loader',use: 'css-loader'})
+            }*/
+            //解析图片
+            {
+                test:/.(jpg|jpeg|png|gif|svg)$/,
+                use:['url-loader?limit=8192&name=./[name].[ext]']
+            },
+            //
+            {
+                test:/.less$/,
+                use:['style-loader','css-loader','less-loader']
+            }
+        ]
     },
-    resolve:{
+     /*resolve:{
 
     },
     babel:{
@@ -32,5 +47,5 @@ module.exports = {
     },
     devServer:{
 
-    },
+    },*/
 }
